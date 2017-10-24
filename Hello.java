961945aad650990d0cb258f45532a5a3d3618ab9
lw <195323046@qq.com>
2017-10-24 09:21:51 +0800

    修改Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("123");
-		System.out.println("222");
+		System.out.println("435435");
+		System.out.println("2232");
 	}
 }
